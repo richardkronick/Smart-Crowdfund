ecbdcddf60f5b2752aecc37e20a79ad60f4cbb2d
Richard Kronick <41272573+richardkronick@users.noreply.github.com>
2022-05-22 17:15:19 -0400

    Updated the github repo link

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ export default () => {
     return (
         <div className='row justify-content-md-center' style={{ marginTop: '6rem', marginBottom: '2rem' }}>
             <Button basic>Created with React, Javascript and Solidity on the Ethereum Rinkeby Blockchain by <a href='https://www.richardkronick.com' target='_blank'>
-                Richard Kronick</a>. View the <a href='https://github.com/richardkronick/smart-campaigner' target='_blank'>code at Github</a>.
+                Richard Kronick</a>. View the <a href='https://github.com/richardkronick/Smart-Crowdfund' target='_blank'>code at Github</a>.
             </Button>
         </div>
     );
